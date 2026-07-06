d2df205b0925e26f61de8acbd6bee0ff60c42d9b
Ash <akisan862@gmail.com>
2026-07-05 18:28:13 -0600

    Replazo de los var y cmabio de funciones

--- viajes.js
+++ viajes.js
@@ -2,12 +2,11 @@
 // viajes.js
 
 // Array para guardar los destinos
-var destinos = [];
+const destinos = [];
 
 // Función para registrar un destino de viaje
-function registrarDestino(destino, fecha, transporte) {
-    // TODO: Crear un objeto con los datos del destino
-    var nuevoViaje = {
+const registrarDestino = (destino, fecha, transporte) => {
+    const nuevoViaje = {
         destino: destino,
         fecha: fecha,
         transporte: transporte,
@@ -15,11 +14,10 @@ function registrarDestino(destino, fecha, transporte) {
     };
 
     destinos.push(nuevoViaje);
-}
-
+};
 // Función para calcular el costo del viaje
-function calcularCosto(destino, transporte) {
-    var costoBase = 0;
+    const calcularCosto = (destino, transporte) => {
+    let costoBase = 0;
 
     // Costo base por destino
     if (destino === "Paris") {
@@ -29,22 +27,20 @@ function calcularCosto(destino, transporte) {
     } else if (destino === "New York") {
         costoBase = 600;
     }
-
     // Costo adicional por tipo de transporte
-    if (transporte === "Avión") {
+  if (transporte === "Avión") {
         costoBase += 200;
     } else if (transporte === "Tren") {
         costoBase += 100;
     }
 
     return costoBase;
-}
 
 // Función para mostrar el itinerario de los viajes registrados
-function mostrarItinerario() {
+    const mostrarItinerario = () => {
     // TODO: Recorrer el arreglo de destinos y mostrar la información de cada uno
-    for (var i = 0; i < destinos.length; i++) {
-        var viaje = destinos[i];
+    for (let i = 0; i < destinos.length; i++) {
+        const viaje = destinos[i];
         console.log("Destino: " + viaje.destino);
         console.log("Fecha: " + viaje.fecha);
         console.log("Transporte: " + viaje.transporte);
