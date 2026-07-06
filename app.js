58a8c4d9d8869079469db8092dfce793bf92a579
Ash <akisan862@gmail.com>
2026-07-05 18:20:00 -0600

    agregando el codigo del drive

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+
+//Iniciar la aplicación
+function iniciarApp() {
+    // Ejemplo de cómo registrar destinos
+    registrarDestino("Paris", "2024-06-15", "Avión");
+    registrarDestino("Londres", "2024-07-01", "Tren");
+
+    // Mostrar el itinerario de los viajes
+    mostrarItinerario();
+}
+
+//Ejecutar la aplicación
+iniciarApp();
